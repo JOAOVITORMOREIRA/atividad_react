05985377b7796e1f578e65207a3809506ec138d6
João Vitor Moreira <joaoitagv@gmail.com>
2023-05-11 17:06:07 -0300

    Atividade React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+function Home(){
+    return(
+    <>
+        <h1 className='titulo'>Hello World React!</h1>
+        <img className='img' scr="" alt="" ></img>
+    </>
+    )
+}
+
+export default Home;
